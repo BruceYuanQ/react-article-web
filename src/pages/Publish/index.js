328261c7c73bcef9725afe372343adf31780802b
yuanzq <yuanzq@asiainfo-sec.com>
2024-05-30 12:26:26 +0800

    编辑文章模块

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -11,12 +11,12 @@ import {
   message
 } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import './index.scss'
 import ReactQuill from 'react-quill'
 import 'react-quill/dist/quill.snow.css'
 import { useEffect, useState } from 'react'
-import { createArticle, getChannelAPI } from '@/apis/article'
+import { createArticle, getArticleById, getArticleListAPI, getChannelAPI, updateArticleAPI } from '@/apis/article'
 import { useChannel } from '@/hooks/useChannel'
 
 const { Option } = Select
@@ -36,11 +36,23 @@ const Publish = () => {
       type: 1,
       cover: {
         type: imageType, //封面模式
-        images: imageList.map(item => item.response.data.url) //图片列表
+        //新增和编辑不同的url处理逻辑
+        images: imageList.map(item => {
+          if (item.response) {
+            return item.response.data.url
+          } else {
+            return item.url
+          }
+        }) //图片列表
       }
     }
-    await createArticle(params)
-    message.success('发布文章成功')
+    //新增 新增接口 编辑状态 更新接口 id
+    if (articleId) {
+      await updateArticleAPI({ ...params, id: articleId })
+    } else {
+      await createArticle(params)
+    }
+    message.success(`${articleId ? '编辑文章成功' : '发布文章成功'}`)
   }
 
   //上传图片
@@ -57,13 +69,34 @@ const Publish = () => {
     setImageType(e.target.value)
   }
 
+  //回填数据
+  const [searchParams] = useSearchParams()
+  const articleId = searchParams.get('id')
+  //获取实例
+  const [form] = Form.useForm()
+  useEffect(() => {
+    async function getArticle() {
+      const res = await getArticleById(articleId)
+      const { cover, ...formValue } = res.data
+      //设置表单数据
+      form.setFieldsValue({ ...formValue, type: cover.type })
+      // 2. 回填封面图片
+      setImageType(cover.type) // 封面类型
+      setImageList(cover.images.map(url => ({ url }))) // 封面list
+    }
+    if (articleId) {
+      getArticle()
+    }
+  }, [articleId, form]
+  )
+
   return (
     <div className="publish">
       <Card
         title={
           <Breadcrumb items={[
             { title: <Link to={'/'}>首页</Link> },
-            { title: '发布文章' },
+            { title: `${articleId ? '编辑文章' : '发布文章'}` },
           ]}
           />
         }
@@ -73,6 +106,7 @@ const Publish = () => {
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 0 }}
           onFinish={onFinish}
+          form={form}
         >
           <Form.Item
             label="标题"
@@ -110,6 +144,7 @@ const Publish = () => {
                 action={'http://geek.itheima.net/v1_0/upload'}
                 onChange={onUploadChange}
                 maxCount={imageType}
+                fileList={imageList}
               >
                 <div style={{ marginTop: 8 }}>
                   <PlusOutlined />
