b0e4580cfc508afbb5bb120068941c542c7d7d73
yuanzq <yuanzq@asiainfo-sec.com>
2024-05-30 12:50:48 +0800

    cdn配置+路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,14 @@ import { createBrowserRouter } from 'react-router-dom'
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
 import AuthRoute from '@/components/AuthRoute'
-import Home from '@/pages/Home'
-import Article from '@/pages/Article'
-import Publish from '@/pages/Publish'
+// import Home from '@/pages/Home'
+// import Article from '@/pages/Article'
+// import Publish from '@/pages/Publish'
+import { Suspense, lazy } from 'react'
+
+const Home = lazy(() => import('@/pages/Home'))
+const Publish = lazy(() => import('@/pages/Publish'))
+const Article = lazy(() => import('@/pages/Article'))
 
 const router = createBrowserRouter([
   {
@@ -14,15 +19,15 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />
+        element: <Suspense fallback={'加载中'}><Home /></Suspense>
       },
       {
         path: 'article',
-        element: <Article />
+        element: <Suspense fallback={'加载中'}><Article /></Suspense>
       },
       {
         path: 'publish',
-        element: <Publish />
+        element: <Suspense fallback={'加载中'}><Publish /></Suspense>
       }
     ]
   },
